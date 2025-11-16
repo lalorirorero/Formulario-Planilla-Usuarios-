5fac99da3a14a3e86bb668ead8bbe1157b650a6e
lalorirorero <129010305+lalorirorero@users.noreply.github.com>
2025-11-16 00:47:36 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,10 @@
-import React from 'react';import ReactDOM from 'react-dom/client';import App from './App';ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css"; // 👈 IMPORTANTE
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
